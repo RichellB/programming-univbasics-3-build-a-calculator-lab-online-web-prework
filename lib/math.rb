31ed9b485e60c45d2964af4274f9701540d8c6f1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-08-18 22:19:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,38 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
+
 
 def subtraction(num1, num2)
+  diff = num2 - num1
+  diff
 
 end
+
 
 def division(num1, num2)
+  div = num2 / num1
+  div
 
 end
+
 
 def multiplication(num1, num2)
+  quotient = num1 * num2
+  quotient
 
 end
+
 
 def modulo(num1, num2)
+  div_mod = num2 % num1
+  div_mod
 
 end
 
+
 def square_root(num)
+  sqr_rt = num.Math.sqrt
 
 end
